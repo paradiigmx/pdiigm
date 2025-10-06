99d96e49c033c44a6723672fbb3f593b5c23b3fd
paradiigmx <paradiigmx@gmail.com>
2025-10-05 18:28:44 -0700

    Delete pages directory

--- pages/contact.tsx
+++
@@ -1,9 +0,0 @@
-export default function Contact() {
-  return (
-    <div className="card">
-      <h1 style={{marginTop:0}}>Contact</h1>
-      <p>Email: hello@paradiigm.net</p>
-      <p>Update this content in <code>/pages/contact.tsx</code>.</p>
-    </div>
-  );
-}
